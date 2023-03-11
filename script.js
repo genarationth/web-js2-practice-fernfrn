92eae8c055b46f8a54715ea6d7af993e50d9a1a9
Fern Piya S <125846412+fernfrn@users.noreply.github.com>
2023-03-11 12:17:29 +0700

    Create script.js
    Submit homework JS-2

---
+++ script.js
@@ -0,0 +1,13 @@
+//homework JS-2
+
+let birthYear = 1991;
+let futureYear = 2030;
+ageCalculator = futureYear-birthYear;
+console.log(ageCalculator);
+
+let person = {
+    Name: 'John',
+    Location: 'Canada',
+    Hobby: 'Hiking'
+}
+console.log(`Hi, My name is ${person.Name}, I live in ${person.Location} and enjoy ${person.Hobby}`);
